d7492e7cf1ea70f1581924687e85b671a98841d6
rithish1224 <magadhana1224@gmail.com>
2025-10-08 22:35:30 +0530

    Set BrowserRouter basename for GitHub Pages routing

--- campus-nest/client/src/index.js
+++ campus-nest/client/src/index.js
@@ -1,11 +1,15 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/CAMPUS-NEST">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
